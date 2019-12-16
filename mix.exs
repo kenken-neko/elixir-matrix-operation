9e1f3cd2309dfdae758a69a3dd2c180dc56b8251
kenken-neko <42142120+kenken-neko@users.noreply.github.com>
2019-12-16 15:48:09 +0900

    Add files via upload

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule MatrixOperation.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :matrix_operation,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      description: "Matrix operation library",
+      start_permanent: Mix.env() == :prod,
+      package: [
+        maintainers: ["tanaka kenta"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/kenken-neko/elixir-matrix-operation"}
+      ],
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [{:ex_doc, "~> 0.10", only: :dev}]
+  end
+end
