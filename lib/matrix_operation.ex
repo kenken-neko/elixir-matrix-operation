8367ba2855b56dbb2a68e1eeccf481dfc17fd922
kenken-neko <42142120+kenken-neko@users.noreply.github.com>
2020-01-04 17:31:31 +0900

    Update matrix_operation.ex

--- lib/matrix_operation.ex
+++ lib/matrix_operation.ex
@@ -5,7 +5,6 @@ defmodule MatrixOperation do
 
   @doc """
     Numbers of row and column of a matrix are got.
-
     #### Examples
       iex> MatrixOperation.row_column_matrix([[3, 2, 3], [2, 1, 2]])
       [2, 3]
@@ -27,8 +26,25 @@ defmodule MatrixOperation do
 
 
   @doc """
-  	A element of a matrix is got.
+    A n-th unit matrix is got.
+    #### Examples
+      iex> MatrixOperation.unit_matrix(3)
+      [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    """
+  def unit_matrix(n) when n > 0 and is_integer(n) do
+    index_list = Enum.to_list(1..n)
+    Enum.map(index_list, fn x -> Enum.map(index_list, & unit_matrix_sub(x, &1)) end)
+  end
+  defp unit_matrix_sub(i, j) when i == j do
+    1
+  end
+  defp unit_matrix_sub(i, j) do
+    0
+  end
 
+
+  @doc """
+  	A element of a matrix is got.
     #### Examples
       iex> MatrixOperation.get_one_element([[1, 2, 3], [4, 5, 6], [7, 8, 9] ], [1, 1])
       1
@@ -42,7 +58,6 @@ defmodule MatrixOperation do
 
   @doc """
     A row of a matrix is got.
-
     #### Examples
       iex> MatrixOperation.get_one_row([[1, 2, 3], [4, 5, 6], [7, 8, 9] ], 1)
       [1, 2, 3]
@@ -55,7 +70,6 @@ defmodule MatrixOperation do
 
   @doc """
     A column of a matrix is got.
-
     #### Examples
         iex> MatrixOperation.get_one_column([[1, 2, 3], [4, 5, 6], [7, 8, 9] ], 1)
         [1, 4, 7]
@@ -69,7 +83,6 @@ defmodule MatrixOperation do
 
   @doc """
   	A row of a matrix is deleted.
-
     #### Examples
         iex> MatrixOperation.delete_one_row([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3)
         [[1, 2, 3], [4, 5, 6]]
@@ -84,7 +97,6 @@ defmodule MatrixOperation do
 
   @doc """
   	Transpose of a matrix
-
     #### Examples
         iex> MatrixOperation.transpose([[1.0, 2.0], [3.0, 4.0]])
         [[1.0, 3.0], [2.0, 4.0]]
@@ -96,7 +108,6 @@ defmodule MatrixOperation do
 
   @doc """
     Trace of a matrix
-
     #### Examples
         iex> MatrixOperation.trace([[1.0, 2.0], [3.0, 4.0]])
         5.0
@@ -117,7 +128,6 @@ defmodule MatrixOperation do
 
   @doc """
     A determinant of a n×n square matrix is got.
-
     #### Examples
         iex> MatrixOperation.determinant([[1, 2, 1], [2, 1, 0], [1, 1, 2]])
         -5
@@ -159,19 +169,19 @@ defmodule MatrixOperation do
 
   @doc """
     Cramer's rule
-
     #### Examples
-        iex> MatrixOperation.cramer([1, 0, 0], [[1, 0, 0], [0, 1, 0], [0, 0, 1]], 0)
+        iex> MatrixOperation.cramer([[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[1], [0], [0]], 0)
         1.0
     """
-  def cramer(t, a, select_index) do
+  def cramer(a, vertical_vec, select_index) do
+    [t] = transpose(vertical_vec)
     det_a = determinant(a)
-    cramer_sub(t, a, select_index, det_a)
+    cramer_sub(a, t, select_index, det_a)
   end
 
   defp cramer_sub(_, _, _, nil), do: nil
   defp cramer_sub(_, _, _, 0), do: nil
-  defp cramer_sub(t, a, select_index, det_a) do
+  defp cramer_sub(a, t, select_index, det_a) do
     rep_det_a = transpose(a) |> replace_element_in_list(select_index, t, 0, []) |> determinant
     rep_det_a / det_a
   end
@@ -186,35 +196,35 @@ defmodule MatrixOperation do
 
 
   @doc """
-    Liner equations are solved.
-
+    Linear equations are solved.
     #### Examples
-        iex> MatrixOperation.linear_equations([1, 0, 0], [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        iex> MatrixOperation.linear_equations([[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[1], [0], [0]])
         [1.0, 0.0, 0.0]
-        iex> MatrixOperation.linear_equations([3, -7, 4], [[0, -2, 1], [-1, 1, -4], [3, 3, 1]])
+        iex> MatrixOperation.linear_equations([[0, -2, 1], [-1, 1, -4], [3, 3, 1]], [[3], [-7], [4]])
         [2.0, -1.0, 1.0]
     """
-  def linear_equations(t, a) do
+  def linear_equations(a, vertical_vec) do
+    [t] = transpose(vertical_vec)
     det_a = determinant(a)
-    condition_det(t, a, det_a)
+    condition_det(a, t, det_a)
   end
   defp condition_det(_, _, 0) do
     nil
   end
-  defp condition_det(t, a, det_a) do
-    linear_equations_sub(t, a, 0, [])
+  defp condition_det(a, t, det_a) do
+    linear_equations_sub(a, t, 0, [])
   end
-  defp linear_equations_sub(t, a, i, output) when i < length(a) do
-    linear_equations_sub(t, a, i + 1, output ++ [cramer(t, a, i)])
+  defp linear_equations_sub(a, t, i, output) when i < length(a) do
+    vertical_vec = transpose([t])
+    linear_equations_sub(a, t, i + 1, output ++ [cramer(a, vertical_vec, i)])
   end
-  defp linear_equations_sub(t, a, i, output) when i == length(a) do
+  defp linear_equations_sub(a, t, i, output) when i == length(a) do
     output
   end
 
 
   @doc """
     A matrix is multiplied by a constant.
-
     #### Examples
         iex> MatrixOperation.const_multiple(-1, [1.0, 2.0, 3.0])
         [-1.0, -2.0, -3.0]
@@ -231,7 +241,6 @@ defmodule MatrixOperation do
 
   @doc """
     Inverse Matrix
-
     #### Examples
         iex> MatrixOperation.inverse_matrix([[1, 1, -1], [-2, -1, 1], [-1, -2, 1]])
         [[-1.0, -1.0, 0.0], [-1.0, 0.0, -1.0], [-3.0, -1.0, -1.0]]
@@ -267,7 +276,6 @@ defmodule MatrixOperation do
 
   @doc """
     Matrix product
-
     #### Examples
         iex> MatrixOperation.product([[3, 2, 3], [2, 1, 2]], [[2, 3], [2, 1], [3, 5]])
         [[19, 26], [12, 17]]
@@ -296,7 +304,6 @@ defmodule MatrixOperation do
 
   @doc """
     Matrix addition
-
     #### Examples
         iex> MatrixOperation.add([[3, 2, 3], [2, 1, 2]], [[2, 3, 1], [3, 2, 2]])
         [[5, 5, 4], [5, 3, 4]]
@@ -321,7 +328,6 @@ defmodule MatrixOperation do
 
   @doc """
     Hadamard product
-
     #### Examples
         iex> MatrixOperation.hadamard_product([[3, 2, 3], [2, 1, 2]], [[2, 3, 1], [3, 2, 2]])
         [[6, 6, 3], [6, 2, 4]]
@@ -339,7 +345,6 @@ defmodule MatrixOperation do
 
   @doc """
     Matrix subtraction
-
     #### Examples
         iex> MatrixOperation.subtract([[3, 2, 3], [2, 1, 2]], [[2, 3, 1], [3, 2, 2]])
         [[1, -1, 2], [-1, -1, 0]]
@@ -364,7 +369,6 @@ defmodule MatrixOperation do
 
   @doc """
     Hadamard division
-
     #### Examples
         iex> MatrixOperation.hadamard_division([[3, 2, 3], [2, 1, 2]], [[2, 3, 1], [3, 2, 2]])
         [[1.5, 0.6666666666666666, 3.0], [0.6666666666666666, 0.5, 1.0]]
@@ -382,7 +386,6 @@ defmodule MatrixOperation do
 
   @doc """
     Hadamard power
-
     #### Examples
         iex> MatrixOperation.hadamard_power([[3, 2, 3], [2, 1, 2]], 2)
         [[9.0, 4.0, 9.0], [4.0, 1.0, 4.0]]
@@ -394,7 +397,6 @@ defmodule MatrixOperation do
 
   @doc """
     Tensor product
-
     #### Examples
         iex> MatrixOperation.tensor_product([[3, 2, 3], [2, 1, 2]], [[2, 3, 1], [2, 1, 2], [3, 5, 3]])
         [
@@ -430,10 +432,12 @@ defmodule MatrixOperation do
       [4.561552806429505, 0.43844714673139706, 1.0000000468390973]
       iex> MatrixOperation.eigenvalue([[1, 2, 3], [2, 1, 3], [3, 2, 1]])
       [5.999999995559568, -2.000000031083018, -0.99999996447655]
-  """
+    """
+  # 2×2 algebra method
   def eigenvalue([[a11, a12], [a21, a22]]) do
     quadratic_formula(1, -a11-a22, a11*a22-a12*a21)
   end
+  # 3×3 algebratic method
   def eigenvalue([[a11, a12, a13], [a21, a22, a23], [a31, a32, a33]]) do
     a = -1
     b = a11 + a22 + a33
@@ -441,23 +445,27 @@ defmodule MatrixOperation do
     d = a11*a22*a33 + a12*a23*a31 + a13*a32*a21 - a11*a32*a23 - a22*a31*a13 - a33*a21*a12
     cubic_formula(a, b, c, d)
   end
-  defp quadratic_formula_sub(a, b, c) when b*b < 4*a*c do
-    nil
+  # in preparation
+  def eigenvalue(a) do
+    "2×2 or 3×3 matrix only"
   end
   defp quadratic_formula(a, b, c) do
     quadratic_formula_sub(a, b, c)
+  end
+  defp quadratic_formula_sub(a, b, c) when b*b < 4*a*c do
+    nil
   end
   defp quadratic_formula_sub(a, b, c) do
     d = :math.sqrt(b * b - 4 * a * c)
     [0.5*(-b + d)/a, 0.5*(-b - d)/a]
   end
-  defp cubic_formula(a, b, c, d) do
+  def cubic_formula(a, b, c, d) do
     cubic_formula_sub(a, b, c, d)
   end
-  defp cubic_formula_sub(a, b, c, d) when -4*a*c*c*c-27*a*a*d*d+b*b*c*c+18*a*b*c*d-4*b*b*b*d < 0 do
+  def cubic_formula_sub(a, b, c, d) when -4*a*c*c*c-27*a*a*d*d+b*b*c*c+18*a*b*c*d-4*b*b*b*d < 0 do
     nil
   end
-  defp cubic_formula_sub(a, b, c, d) do
+  def cubic_formula_sub(a, b, c, d) do
     ba = b/a
     ca = c/a
     da = d/a
@@ -474,36 +482,78 @@ defmodule MatrixOperation do
 
     [x1, x2, x3]
   end
-  defp cubic_formula_sub2(x) when x < 0 do
+  def cubic_formula_sub2(x) when x < 0 do
     [0, :math.sqrt(-x)]
   end
-  defp cubic_formula_sub2(x) do
+  def cubic_formula_sub2(x) do
     [:math.sqrt(x), 0]
   end
-  defp atan(x) when x < 0 do
+  def atan(x) when x < 0 do
     y = atan(-x)
     -1 * y
   end
-  defp atan(x) do
+  def atan(x) do
     atan_sub(x, 0, 0)
   end
-  defp atan_sub(x, z, s) when z < x do
+  def atan_sub(x, z, s) when z < x do
     del = 0.0000001
     z = z + del
     s = s + del/(z*z+1)
     atan_sub(x, z, s)
   end
-  defp atan_sub(_, _, s) do
+  def atan_sub(_, _, s) do
     s
   end
-  defp csqrt([re, im], n) when re == 0 do
+  def csqrt([re, im], n) when re == 0 do
     nil
   end
-  defp csqrt([re, im], n) do
+  def csqrt([re, im], n) do
     r = :math.pow(re*re+im*im, 0.5/n)
     re2 = r*(:math.cos(atan(im/re)/n))
     im2 = r*(:math.sin(atan(im/re)/n))
     [re2, im2]
+  end
+
+
+  @doc """
+    Power iteration method (maximum eigen value and eigen vector)
+
+    #### Examples
+      iex> MatrixOperation.power_iteration([[3, 1], [2, 2]], 100)
+      [4.0, [0.7071067811865476, 0.7071067811865476]]
+      iex> MatrixOperation.power_iteration([[1, 1, 2], [0, 2, -1], [0, 0, 3]], 100)
+      [3.0, [0.3333333333333333, -0.6666666666666666, 0.6666666666666666]]
+    """
+  def power_iteration(a, max_k) do
+    init_vec = random_column(length(a))
+    xk_pre   = power_iteration_sub(a, init_vec, 1, max_k)
+    # eigen vector
+    [xk_vec]     = product(a, xk_pre) |> transpose
+    [xk_pre_vec] = transpose(xk_pre)
+    # eigen value
+    eigen_value = inner_product(xk_vec, xk_vec)/inner_product(xk_vec, xk_pre_vec)
+    [eigen_value, xk_pre_vec]
+  end
+  defp random_column(num) when num > 1 do
+    random_column_sub(num, 0, [])
+  end
+  defp random_column(num) do
+    nil
+  end
+  defp random_column_sub(num, n, random_list) when num > n do
+    random_column_sub(num, n + 1, random_list ++ [[Enum.random(0..50000)/10000]])
+  end
+  defp random_column_sub(num, n, random_list) do
+    random_list
+  end
+  defp power_iteration_sub(a, v, k, max_k) when max_k >= k do
+    vp    = product(a, v)
+    [vpt] = transpose(vp)
+    vp_norm = const_multiple(1/:math.sqrt(inner_product(vpt, vpt)), vp) # for overflow suppression
+    power_iteration_sub(a, vp_norm, k + 1, max_k)
+  end
+  defp power_iteration_sub(_, v, _, _) do
+    v
   end
 
 end
