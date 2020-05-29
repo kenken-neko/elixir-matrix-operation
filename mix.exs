d46393a79da2db733d63d9b2ae04ca857499d841
kenken-neko <42142120+kenken-neko@users.noreply.github.com>
2020-05-30 00:17:37 +0900

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MatrixOperation.MixProject do
   def project do
     [
       app: :matrix_operation,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       description: "Matrix operation library",
       start_permanent: Mix.env() == :prod,
