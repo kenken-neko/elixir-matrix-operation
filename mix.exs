617b11de1e963e5fdbdc41d1f64f72fd7647971c
kenken_nako <sc421223e@gmail.com>
2021-12-02 23:25:15 +0900

    Hex version up

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MatrixOperation.MixProject do
   def project do
     [
       app: :matrix_operation,
-      version: "0.4.9",
+      version: "0.4.10",
       elixir: "~> 1.12.3",
       description: "Matrix operation library",
       start_permanent: Mix.env() == :prod,
