98c1c9424d05d9497d82a24eaa8c38efe07e7ff1
Kenta Tanaka <42142120+kenken-neko@users.noreply.github.com>
2022-01-09 22:42:44 +0900

    Update matrix_operation_test.exs

--- test/matrix_operation_test.exs
+++ test/matrix_operation_test.exs
@@ -2,4 +2,17 @@ defmodule MatrixOperationTest do
   use ExUnit.Case
 
   doctest MatrixOperation
+  import MatrixOperation
+
+  describe "inverse_matrix" do
+    test "property" do
+      a = [[1, 1, -1], [-2, -1, 1], [-1, -2, 1]]
+      result = a
+      |> MatrixOperation.inverse_matrix()
+      |> product(a)
+
+      expect = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+      assert result == expect
+    end
+  end
 end
